597b22d32b4806649d4a5fa575046b15b1ee576c
vikrampatil41 <vikramhpatil1992@gmail.com>
2024-02-10 21:10:17 +0530

    End of section 5

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,31 +4,24 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  model: any ={};
-  loggedIn = false;
-  constructor(private accountService: AccountService) {  
-    
-  }
+  model: any = {};
+  constructor(public accountService: AccountService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  login(){
-    this.accountService.login(this.model).subscribe(
-      {
-        next: response => {
-          console.log(response);
-          this.loggedIn = true;
-        },
-        error: error => console.log(error)
-      })    
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => console.log(error),
+    });
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
-
 }
